c7f3084bd866f5ea7770fcaa2d4fac5bb74f6171
Kushal <kushal_pandya96@hotmail.com>
2017-03-14 22:08:38 -0400

    got t (text) tag working

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# CIS2750_A3
-A3 for 2750
+
+
+
+THINGS THAT DONT WORK
+-----------------------
+
+C Program
+- tags that are not specified in assignment will not work
+- default attributes will not work, only specified attributes will convert
+
--- a3.c
+++ a3.c
@@ -12,33 +12,111 @@
 #include <string.h>
 #include <unistd.h>
 
-
+int getIndexOfChar(char * string, char c);
 int getInfo(FILE *infile, char tag);
 char *getAttribute(FILE *infile, char *tag);
+int removeCharFromString(char * string, char c);
 
 
+char **getArgs(char *argString) {
+
+	int i;
+	char *newStr;
+	char *token;
+
+	/*Creating args array*/
+	char **args = malloc(sizeof(char*)*10);
+	for (i=0; i<10; i++)
+		args[i] = calloc(100, sizeof(char));
+
+
+	strtok(argString, "(");
+	newStr = strtok(NULL, "(");
+	removeCharFromString(newStr, ')');
+
+	if (strchr(newStr, ',') == NULL) {
+		strcpy(args[0], newStr);
+		return args;
+	}
+
+	i = 0;
+	token = strtok(newStr, ",");
+	while(token != NULL) {
+
+		strcpy(args[i], token);
+		token = strtok(NULL, ",");
+		i++;
+	}
+	
+	return args;
+}
+
+
+void tConvert(char **args) {
+
+	int i;
+	char *attribute;
+	char *value;
+	char buffer[500];
+
+	for (i=0; i<10; i++) {
+		attribute = strtok(args[i], "=");
+		value = strtok(NULL, "=");
+
+		if (value != NULL) {
+			if (strcmp(attribute, "text") == 0)
+				printf("%s", value);
+			else if (strcmp(attribute, "file") == 0) {
+
+				value[strcspn(value, "\r\n")] = 0;
+				FILE *fp = fopen(value, "r");
+				if (fp == NULL) {
+					printf("ERROR FILE NOT FOUND '%s'\n", value);
+					exit(0);
+				}
+
+				while (fgets(buffer, 500, fp) != NULL) {
+					printf("%s", buffer);
+				}
+			}	
+		}
+	}
+}
 
 void parseFile() {
 	
+	int i;
 	char buffer[500];
 	char tag;
-	unsigned long position = 0;
+	char **args;
 
 	FILE *infile = fopen("format.txt", "r");
 
 	while (fgets(buffer, 500, infile) != NULL) {
 
-		if (strcmp(buffer, "\n") != 0) {
+		if (strcmp(buffer, "\n") != 0 && buffer[1] == '(') {
 			tag = buffer[0];
+
+			args = getArgs(buffer);
 
 			switch(tag) {
 				case 'd':
 					printf("\t<hr>\n");
+					break;
+				case 't':
+					tConvert(args);
+
+				default:
+					break;
 
 
 			}
 		}
 	}
+
+	for (i=0; i<10; i++)
+		free(args[i]);
+	free(args);
 
 	fclose(infile);
 }
@@ -56,6 +134,8 @@ void formatFile(FILE *infile) {
 			case '.':
 				if (inBrackets == 0 && inString == 0)
 					fprintf(tempfile, "\n");
+				if (inString == 1 && inBrackets == 1)
+					fprintf(tempfile, ".");					
 				break;
 			case '(': 
 				if (inString == 0)
@@ -95,5 +175,24 @@ int main(int argc, char *argv[]) {
 	printf("</html>\n");
 	fclose(fp);
 
+	return 0;
+}
+
+int getIndexOfChar(char * string, char c) {
+	const char *ptr = strchr(string, c);
+	int index;
+
+	if (ptr) 
+		return index = ptr - string;
+	return -1;
+}
+
+int removeCharFromString(char * string, char c) {
+	int result = getIndexOfChar(string, c);
+
+	if (result >= 0) {
+		memmove(&string[result], &string[result + 1], strlen(string) - result);
+		return 1;
+	}
 	return 0;
 }
